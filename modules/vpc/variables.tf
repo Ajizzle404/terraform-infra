dd4cb6eeae0897a6c10ce5d686847c509dfa13d0
Adrian <adriantate83@gmail.com>
2026-06-14 18:00:35 -0400

    feat

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+//  default     = "10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  description = "The tenancy option for instances launched into the VPC"
+  default     = "default"
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  default     = "Dev-vpc"
+}
